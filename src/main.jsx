53be9601ae3e6b45fdfc906bc136551e08202ba6
kimyh <ikimyh059554@gmail.com>
2025-04-04 11:19:12 +0900

    chore: main.jsx에 안쓰이는 코드 주석 처리

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import Counter from "./test.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")).render(<App />);
+//createRoot(document.getElementById("root")).render(<Counter />);
