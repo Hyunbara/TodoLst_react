166780e5b26d85c3ee6533e7e519bc6e9ee9a932
Hyun <ikimyh059554@gmail.com>
2025-04-06 21:05:49 +0900

    feat: input 탭 엔터 또는 버튼 클릭 시 할 일 추가 기능 구현

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const TodoInput = ({ date, addTodo }) => {
-  return <div>todoInput</div>;
+  const [text, setText] = useState("");
+
+  // input 탭에서 엔터 시 또는 버튼을 눌렀을 때 동작하는 함수 (할 일 추가 함수)
+  const handleAdd = () => {
+    // 만약 값이 없으면 함수 종료
+    if (!text.trim()) return;
+
+    addTodo(date, text);
+    setText("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        placeholder="할 일을 입력하세요."
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleAdd();
+        }}
+      />
+      <button onClick={handleAdd}>추가하기</button>
+    </div>
+  );
 };
 
 export default TodoInput;
