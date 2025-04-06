0c6d972320051d3828b148f4622c4762dce55cb3
Hyun <ikimyh059554@gmail.com>
2025-04-06 21:57:02 +0900

    feat: 입력 폼(할 일추가) 기능 구현

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,15 +1,17 @@
 import TodoInput from "../components/TodoInput";
+import "../styles/todoPage.scss";
 
 const Today = ({ todos, addTodo, toggleTodo, deleteTodo }) => {
   return (
     <>
       <h2>🎰 오늘의 할일</h2>
       <TodoInput date="today" addTodo={addTodo} />
-      <ul>
+      <ul className="todo-list">
         {todos.map((todo) => (
           <li key={todo.id}>
             <input type="checkbox" checked={todo.completed} onChange={() => toggleTodo("today", todo.id)} />
-            <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>{todo.text}</span>
+            {/* <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>{todo.text}</span> */}
+            <span className={todo.completed ? "completed" : ""}>{todo.text}</span>
             <button onClick={() => deleteTodo("today", todo.id)}>삭제</button>
           </li>
         ))}
