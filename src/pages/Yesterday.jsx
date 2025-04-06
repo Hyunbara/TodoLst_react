526981087bc0740d9fb23bb7f3cd856dc2d42147
Hyun <ikimyh059554@gmail.com>
2025-04-06 21:57:48 +0900

    feat: 할 일 컴포넌트 쪼개어 추가(진행중)

--- src/pages/Yesterday.jsx
+++ src/pages/Yesterday.jsx
@@ -1,15 +1,18 @@
 import TodoInput from "../components/TodoInput";
+import "../styles/todoPage.scss";
+import TodoPercentage from "../components/TodoPercentage";
 
 const Yesterday = ({ todos, addTodo, toggleTodo, deleteTodo }) => {
   return (
     <>
       <h2>🎉 어제 했던 일</h2>
       <TodoInput date="yesterday" addTodo={addTodo} />
-      <ul>
+      <TodoPercentage date="yesterday" todos={todos} />
+      <ul className="todo-list">
         {todos.map((todo) => (
           <li key={todo.id}>
             <input type="checkbox" checked={todo.completed} onChange={() => toggleTodo("yesterday", todo.id)} />
-            <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>{todo.text}</span>
+            <span className={todo.completed ? "completed" : ""}>{todo.text}</span>
             <button onClick={() => deleteTodo("yesterday", todo.id)}>삭제하기</button>
           </li>
         ))}
