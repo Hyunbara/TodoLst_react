fa4d300e1ee6168ab87c177d70ae7ea07601c45c
Hyun <ikimyh059554@gmail.com>
2025-04-08 22:53:49 +0900

    feat: 어제/오늘/내일 별 할 일 완수율 보여주는 컴포넌트 기능 개발발

--- src/components/TodoPercentage.jsx
+++ src/components/TodoPercentage.jsx
@@ -1,19 +1,26 @@
 //date로 받고, 전체 중에 complete 비율을 보여주는 컴포넌트
 
+import { useMemo } from "react";
+
 // TodoPercentage 컴포넌트의 리턴값의 코드가 복잡해져서 따로 뺐음.
 const TodoDate = ({ date, percentage }) => {
   const todoDate = date == "today" ? "오늘" : date == "yesterday" ? "어제" : date == "tomorrow" ? "내일" : "";
 
   return (
     <div>
-      {todoDate}의 달성률은 {percentage}입니다.
+      {todoDate}의 달성률은 {percentage}%입니다.
     </div>
   );
 };
 
 const TodoPercentage = ({ date, todos }) => {
-  const percentage = 10;
-  // useMemo 사용해보기
+  const percentage = useMemo(() => {
+    if (todos.length === 0) return 0;
+
+    const completeCount = todos.filter((todo) => todo.completed).length;
+
+    return Math.round((completeCount / todos.length) * 100);
+  }, [todos]);
 
   return (
     <div>
